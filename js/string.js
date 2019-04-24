e1fccc1e60cfff33029665106e0ac6f42144e804
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-04-23 19:49:59 -0700

    learning string data type

---
+++ js/string.js
@@ -0,0 +1,23 @@
+// string data type
+var str1 = 'It\'s Tuesday today!';
+console.log(str1);
+
+var str2 = "We are learning Javascript today!";
+console.log(str2);
+
+var firstName = "kristin"; // camelCase naming convention; main variables in project
+console.log(firstName);
+
+var last_name = "bonillas"; // snek naming convention; object key names
+console.log(last_name);
+
+
+
+var fullName = firstName + ' ' + last_name; // string concatination
+console.log(fullName);
+
+var fullName = firstName.concat(' ', last_name); //other way
+console.log(fullName);
+
+var fullName = firstName.concat(' ').concat(last_name); //other other way
+console.log(fullName);
