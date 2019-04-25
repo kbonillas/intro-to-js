2ad3cb96b2ee58797069f30659c25625d6e0aaf2
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-04-25 16:57:36 -0700

    reviewed numbered variables

---
+++ js/review-number.js
@@ -0,0 +1,20 @@
+var a = 5;
+console.log(a);
+var b = 12;
+console.log(b);
+
+
+var sum = a + b;
+console.log(sum);
+
+var diff = b - a;
+console.log(diff);
+
+var mul = a * b;
+console.log(mul);
+
+var div = b / a;
+console.log(div);
+
+var mod = b % a;
+console.log(mod);
