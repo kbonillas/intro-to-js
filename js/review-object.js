3f2cd6b5d03aa04f4a194c95ab5448ea22b177e2
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-04-25 17:35:39 -0700

    added object variables and conactenation for my full name

---
+++ js/review-object.js
@@ -0,0 +1,10 @@
+var fullName = {
+    firstName: 'kristin', // key:value
+    lastName: 'bonillas'
+};
+console.log(fullName);
+console.log(fullName.firstName);
+console.log(fullName.lastName);
+
+var fullNameCat = fullName.firstName.concat(' ', fullName.lastName);
+console.log(fullNameCat);
