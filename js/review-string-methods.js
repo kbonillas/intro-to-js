a3e9e72a09309429040d0efcec202997cc6f31e8
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-04-25 17:12:46 -0700

    added concatination to output car model and make

--- js/review-string-methods.js
+++ js/review-string-methods.js
@@ -3,3 +3,14 @@ console.log(str1);
 
 console.log(str1.slice(0,6));
 console.log(str1.slice(6,12));
+
+
+
+var make = 'subaru';
+var model = 'crosstrek';
+
+var fullCar = make + ' ' + model;
+console.log(fullCar);
+
+var fullCar = make.concat(' ', model);
+console.log(fullCar);
